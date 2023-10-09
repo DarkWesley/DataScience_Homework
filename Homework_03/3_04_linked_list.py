cc260720443e85001d668f76a10792682faf0147
DarkWesley <865373641@qq.com>
2023-10-09 19:21:06 +0800

    accomplished question NO.4 from Chapter.3

---
+++ Homework_03/3_04_linked_list.py
@@ -0,0 +1,83 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+        self.length += 1
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        self.length += 1
+
+    def delete(self, data):
+        if not self.head:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next:
+            if current.next.data == data:
+                current.next = current.next.next
+                return
+            current = current.next
+        self.length -= 1
+
+    def update(self, old_data, new_data):
+        current = self.head
+        while current:
+            if current.data == old_data:
+                current.data = new_data
+                return
+            current = current.next
+
+    def find(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                return True
+            current = current.next
+        return False
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+# 以下是一个示例
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    linked_list.append(1)
+    linked_list.append(2)
+    linked_list.append(3)
+    linked_list.display()  # 输出：1 -> 2 -> 3 -> None
+
+    linked_list.prepend(0)
+    linked_list.display()  # 输出：0 -> 1 -> 2 -> 3 -> None
+
+    linked_list.delete(2)
+    linked_list.display()  # 输出：0 -> 1 -> 3 -> None
+
+    linked_list.update(3, 2)
+    linked_list.display()  # 输出：0 -> 1 -> 2 -> None
+
+    print(linked_list.find(1))  # 输出：True
+    print(linked_list.find(2))  # 输出：False
